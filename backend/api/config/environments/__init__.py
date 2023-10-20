4e6cbf5bbc41c6cb9bd9ba13f4c55e0736500f1d
rifatrakib <Rakib.1508@outlook.com>
2023-10-21 03:11:10 +0600

    added services for running postgres database and pgadmin on containers

--- backend/api/config/environments/__init__.py
+++ backend/api/config/environments/__init__.py
@@ -42,6 +42,17 @@ class BaseConfig(BaseSettings):
     APP_NAME: str
     MODE: Modes
 
+    # SQL Database Configurations
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    # PGAdmin Configurations
+    PGADMIN_DEFAULT_EMAIL: str
+    PGADMIN_DEFAULT_PASSWORD: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
@@ -58,3 +69,12 @@ class BaseConfig(BaseSettings):
         file_secret_settings: PydanticBaseSettingsSource,
     ) -> Tuple[PydanticBaseSettingsSource, ...]:
         return init_settings, env_settings, dotenv_settings, SettingsSource(settings_cls)
+
+    @property
+    def RDS_URI(self) -> str:
+        username = self.POSTGRES_USER
+        password = self.POSTGRES_PASSWORD
+        host = self.POSTGRES_HOST
+        port = self.POSTGRES_PORT
+        db_name = self.POSTGRES_DB
+        return f"postgresql+asyncpg://{username}:{password}@{host}:{port}/{db_name}"
--- manage.py
+++ manage.py
@@ -2,9 +2,23 @@ import subprocess
 from enum import Enum
 from typing import Union
 
+from pydantic import BaseModel
 from typer import Typer
 
 app = Typer()
+
+
+class PostgresConfig(BaseModel):
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+
+class PGAdminConfig(BaseModel):
+    PGADMIN_DEFAULT_EMAIL: str
+    PGADMIN_DEFAULT_PASSWORD: str
 
 
 class Modes(str, Enum):
@@ -21,6 +35,20 @@ def deploy(mode: Union[str, None] = "development"):
 
         with open("backend/.env", "w") as writer:
             writer.write(f"MODE={mode}\n")
+
+        with open(f"backend/secrets/{mode}.json") as reader:
+            secrets = reader.read()
+
+        postgres_secrets = PostgresConfig.model_validate_json(secrets)
+        pgadmin_secrets = PGAdminConfig.model_validate_json(secrets)
+
+        with open("backend/.env.postgres", "w") as writer:
+            for key, value in postgres_secrets.model_dump().items():
+                writer.write(f"{key}={value}\n")
+
+        with open("backend/.env.pgadmin", "w") as writer:
+            for key, value in pgadmin_secrets.model_dump().items():
+                writer.write(f"{key}={value}\n")
 
         subprocess.run("docker compose up --build")
     except KeyError:
